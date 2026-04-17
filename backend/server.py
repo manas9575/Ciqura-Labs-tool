de48ffafcd4eb402feb50627523f55d2626ae383
manas9575 <68996684+manas9575@users.noreply.github.com>
2026-04-17 23:50:15 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1485,8 +1485,13 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "https://ciqura-labs-tool-lznd9ffpc-maa2.vercel.app",
+         "https://ciqura-labs-tool.vercel.app"
+        "http://localhost:3000"
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
